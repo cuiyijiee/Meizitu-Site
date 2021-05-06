563e90d1e9d65565024934fee4ebd7fe8e907124
cuiyijie <cyj976655@gmail.com>
2021-05-06 22:31:55 +0800

    调整广告

--- src/test/java/me/cuiyijie/nongmo/NongmoApplicationTests.java
+++ src/test/java/me/cuiyijie/nongmo/NongmoApplicationTests.java
@@ -1,13 +1,27 @@
 package me.cuiyijie.nongmo;
 
+import me.cuiyijie.nongmo.entity.Album;
+import me.cuiyijie.nongmo.service.AlbumService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class NongmoApplicationTests {
 
+    @Autowired
+    private AlbumService albumService;
+
     @Test
     void contextLoads() {
+        Optional<Album> maybeAlbum = albumService.findById(1000);
+        if (maybeAlbum.isPresent()) {
+            Album album = maybeAlbum.get();
+            System.out.println(album.getAlbumUrl());
+        } else {
+            System.out.println("not found");
+        }
     }
-
 }
