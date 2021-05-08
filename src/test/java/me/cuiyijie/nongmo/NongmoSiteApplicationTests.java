96cfc4c22ad8bb82ac05ff5a0e15d1ea779b03ea
cuiyijie <cyj976655@gmail.com>
2021-05-09 00:12:39 +0800

    更改构建工具gradle为maven

---
+++ src/test/java/me/cuiyijie/nongmo/NongmoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cuiyijie.nongmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongmoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
